c00f1890df26c919de25444f89bb7a5005ddffd9
souzlume <lucassmelo2020@gmail.com>
2025-06-30 18:01:21 -0300

    teste com alert e prompt

---
+++ aula2/index.js
@@ -0,0 +1,3 @@
+const nome = 'Pedro';
+const condicao = 'Bobalhão';
+console.log(`O que ${nome} é? ${condicao}!`);
---
+++ aula3/soma.js
@@ -0,0 +1,6 @@
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número');
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+const resultado = num1 + num2;
+alert('O resultado é: ' + resultado);

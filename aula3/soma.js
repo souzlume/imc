9df86d02a339d2f0e6aba0715f567ce996e8ca04
lucas melo <lucassmelo2020@gmail.com>
2025-06-30 18:09:46 -0300

    aula3 deletada

--- aula3/soma.js
+++
@@ -1,6 +0,0 @@
-let num1 = prompt('Digite um número');
-let num2 = prompt('Digite outro número');
-num1 = parseFloat(num1);
-num2 = parseFloat(num2);
-const resultado = num1 + num2;
-alert('O resultado é: ' + resultado);
